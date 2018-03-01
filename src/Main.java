130cf01e816e3f5796016d285135b9ec93ff25e8
Konstantinos Karatsenidis <karatsenidis@outlook.com>
2018-03-01 22:41:20 +0200

    refactor

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,7 @@
 import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -16,7 +15,7 @@ public class Main {
         long T;
         int R, C, F, N, B;
         Car[] cars = null;
-        ArrayList<Ride> rides = new ArrayList<>();
+        Ride[] rides = null;
 
         try (Scanner scanner = new Scanner(new File(args[0]))) {
             R = scanner.nextInt();
@@ -26,12 +25,12 @@ public class Main {
             B = scanner.nextInt();
             T = scanner.nextLong();
 
-            rides.ensureCapacity(N);
+            rides = new Ride[N];
 
             for (int i = 0; i < N; i++) {
-                rides.add(new Ride(scanner, i));
+                rides[i] = new Ride(scanner, i);
             }
-            Collections.sort(rides);
+            Arrays.sort(rides);
 
             cars = new Car[F];
             for (int i = 0; i < F; i++) {
@@ -42,27 +41,25 @@ public class Main {
             System.exit(-2);
         }
 
-        int best_car;
-        int best_score;
-        for (int i = 0; i < rides.size(); i++) {
-            best_car = 0;
-            best_score = 0;
+        for (Ride ride : rides) {
+            int best_car = 0;
+            int best_score = 0;
             for (int j = 0; j < cars.length; j++) {
-                int score = cars[j].evaluate_ride(rides.get(i));
+                int score = cars[j].evaluate_ride(ride);
                 if (score > best_score) {
                     best_car = j;
                     best_score = score;
                 }
             }
             if (best_score > 0)
-                cars[best_car].assign_ride(rides.get(i));
+                cars[best_car].assign_ride(ride);
         }
 
 
         try (PrintWriter writer = new PrintWriter("output/output.txt")) {
-            for (int i = 0; i < cars.length; i++) {
-                writer.print(cars[i].rides_id.size());
-                for (Integer id : cars[i].rides_id) {
+            for (Car car : cars) {
+                writer.print(car.rides_id.size());
+                for (Integer id : car.rides_id) {
                     writer.print(" " + id);
                 }
                 writer.println();
