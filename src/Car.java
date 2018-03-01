a24a34a2cd4fbac40b94d80e4e7ae21bbf54c416
span1495 <sdi1300007@di.uoa.gr>
2018-03-01 22:00:25 +0200

    ride assign

--- src/Car.java
+++ src/Car.java
@@ -12,13 +12,16 @@ class Car {
         rides_id = new ArrayList<>();
     }
 
-    void assign_ride(Ride ride, int current) {
+    int assign_ride(Ride ride, int current) {
         this.ride = ride;
         this.finishing_time = ride.distance + current;
+        this.current=ride.end;
         rides_id.add(ride.id);
     }
     
-    int evaluate_ride(Ride ride) {
+    int evaluate_ride(Ride ride,int curtime) {
+        if(curtime <= finishing_time)
+            return -1;
     	int evaluation = 0;
     	int curr_vs_start = manhattan_distance(current, ride.start);
     	if(curr_vs_start + ride.distance > ride.latest ) {
